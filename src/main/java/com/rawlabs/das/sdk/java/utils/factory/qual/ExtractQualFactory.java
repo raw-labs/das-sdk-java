7a30f860704e84b3bb7c41879095317ca8301c07
Alex Zerntev <alex.zerntev@raw-labs.com>
2024-10-02 18:34:15 +0300

    wip

--- src/main/java/com/rawlabs/das/sdk/java/utils/factory/qual/ExtractQualFactory.java
+++ src/main/java/com/rawlabs/das/sdk/java/utils/factory/qual/ExtractQualFactory.java
@@ -1,3 +1,103 @@
 package com.rawlabs.das.sdk.java.utils.factory.qual;
 
-public final class ExtractQualFactory {}
+import com.rawlabs.das.sdk.java.utils.factory.value.DefaultExtractValueFactory;
+import com.rawlabs.das.sdk.java.utils.factory.value.ExtractValueFactory;
+import com.rawlabs.protocol.das.Qual;
+
+import java.util.List;
+import java.util.Optional;
+
+public class ExtractQualFactory {
+
+  protected static ExtractValueFactory extractValueFactory = new DefaultExtractValueFactory();
+
+  public static Object extractEq(List<Qual> quals, String fieldName) {
+    Optional<Qual> simpleQuals =
+        quals.stream()
+            .filter(
+                q ->
+                    q.hasSimpleQual()
+                        && (q.getSimpleQual().hasOperator())
+                        && q.getSimpleQual().getOperator().hasEquals()
+                        && q.getFieldName().equals(fieldName))
+            .findFirst();
+    return simpleQuals
+        .map(qual -> extractValueFactory.extractValue(qual.getSimpleQual().getValue()))
+        .orElse(null);
+  }
+
+  public static Object extractNeq(List<Qual> quals, String fieldName) {
+    Optional<Qual> simpleQuals =
+        quals.stream()
+            .filter(
+                q ->
+                    q.hasSimpleQual()
+                        && (q.getSimpleQual().hasOperator())
+                        && q.getSimpleQual().getOperator().hasNotEquals()
+                        && q.getFieldName().equals(fieldName))
+            .findFirst();
+    return simpleQuals
+        .map(qual -> extractValueFactory.extractValue(qual.getSimpleQual().getValue()))
+        .orElse(null);
+  }
+
+  public static Object extractGt(List<Qual> quals, String fieldName) {
+    Optional<Qual> simpleQuals =
+        quals.stream()
+            .filter(
+                q ->
+                    q.hasSimpleQual()
+                        && (q.getSimpleQual().hasOperator())
+                        && q.getSimpleQual().getOperator().hasGreaterThan()
+                        && q.getFieldName().equals(fieldName))
+            .findFirst();
+    return simpleQuals
+        .map(qual -> extractValueFactory.extractValue(qual.getSimpleQual().getValue()))
+        .orElse(null);
+  }
+
+  public static Object extractGte(List<Qual> quals, String fieldName) {
+    Optional<Qual> simpleQuals =
+        quals.stream()
+            .filter(
+                q ->
+                    q.hasSimpleQual()
+                        && (q.getSimpleQual().hasOperator())
+                        && q.getSimpleQual().getOperator().hasGreaterThanOrEqual()
+                        && q.getFieldName().equals(fieldName))
+            .findFirst();
+    return simpleQuals
+        .map(qual -> extractValueFactory.extractValue(qual.getSimpleQual().getValue()))
+        .orElse(null);
+  }
+
+  public static Object extractLt(List<Qual> quals, String fieldName) {
+    Optional<Qual> simpleQuals =
+        quals.stream()
+            .filter(
+                q ->
+                    q.hasSimpleQual()
+                        && (q.getSimpleQual().hasOperator())
+                        && q.getSimpleQual().getOperator().hasLessThan()
+                        && q.getFieldName().equals(fieldName))
+            .findFirst();
+    return simpleQuals
+        .map(qual -> extractValueFactory.extractValue(qual.getSimpleQual().getValue()))
+        .orElse(null);
+  }
+
+  public static Object extractLte(List<Qual> quals, String fieldName) {
+    Optional<Qual> simpleQuals =
+        quals.stream()
+            .filter(
+                q ->
+                    q.hasSimpleQual()
+                        && (q.getSimpleQual().hasOperator())
+                        && q.getSimpleQual().getOperator().hasLessThanOrEqual()
+                        && q.getFieldName().equals(fieldName))
+            .findFirst();
+    return simpleQuals
+        .map(qual -> extractValueFactory.extractValue(qual.getSimpleQual().getValue()))
+        .orElse(null);
+  }
+}
